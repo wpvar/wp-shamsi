58123e0d92fabae86a134e7f553f2d09ae02bd06
Ali Faraji <alifaraji.mail@gmail.com>
2020-09-29 22:22:54 +0330

    Update wpsh_num.js
    Scip triming scripts and styles

--- assets/js/wpsh_num.js
+++ assets/js/wpsh_num.js
@@ -14,10 +14,13 @@ jQuery(document).ready(function() {
 
   function wpsh_num(el) {
     if (el.nodeType == 3) {
+      var parent = jQuery(el.parentElement).prop("tagName");
       var list = el.data.match(/[0-9]/g);
       if (list != null && list.length != 0) {
-        for (var i = 0; i < list.length; i++)
-          el.data = el.data.replace(list[i], persian[list[i]]);
+        for (var i = 0; i < list.length; i++ )
+          if(parent != 'STYLE' && parent != 'SCRIPT') {
+            el.data = el.data.replace(list[i], persian[list[i]]);
+        }
       }
     }
     for (var i = 0; i < el.childNodes.length; i++) {
